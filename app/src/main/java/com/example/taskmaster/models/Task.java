0c5546e6f67213908aa3482d5b147ccf1624f302
valdes642 <vhvergarav@gmail.com>
2026-09-08 18:03:51 -0300

    Update Task.java

--- app/src/main/java/com/example/taskmaster/models/Task.java
+++ app/src/main/java/com/example/taskmaster/models/Task.java
@@ -1,4 +1,18 @@
 package com.example.taskmaster.models;
 
 public class Task {
+    private String title;
+    private String category;
+
+    public Task(String title, String category) {
+        this.title = title;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getCategory() {
+        return category;
+    }
 }
