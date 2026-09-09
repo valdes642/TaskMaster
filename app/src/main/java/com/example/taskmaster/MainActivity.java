454dcb65b6261dd20e47bbbb2c3e3ce544656a7f
valdes642 <vhvergarav@gmail.com>
2026-09-09 19:35:49 -0300

    test

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.taskmaster;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import com.example.taskmaster.R;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,12 +16,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnCreateTask = findViewById(R.id.btnCreateTask);
+        // Aquí iría la inicialización del RecyclerView y el Adapter
+
+        btnCreateTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/taskmaster/activities/AddTaskActivity.java
@@ -0,0 +1,4 @@
+package com.example.taskmaster.activities;
+
+public class AddTaskActivity {
+}
