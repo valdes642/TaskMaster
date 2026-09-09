9bc6c6192568f76de1f1b8a8f41a6a99a97622e1
valdes642 <vhvergarav@gmail.com>
2026-09-09 18:55:58 -0300

    avance 2

--- app/src/main/java/com/example/taskmaster/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/taskmaster/activities/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.taskmaster.activities;
 
 import android.os.Bundle;
 import android.content.Intent;
-
+import android.view.View;
+import android.widget.Button;
+import com.example.taskmaster.R;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
