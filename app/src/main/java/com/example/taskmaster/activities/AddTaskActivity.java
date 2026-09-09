34fe1716f0ab033596673791f225204bf613cc8f
valdes642 <vhvergarav@gmail.com>
2026-09-09 19:48:02 -0300

    AVANCE estoy enfermo

--- app/src/main/java/com/example/taskmaster/activities/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmaster/activities/AddTaskActivity.java
@@ -7,5 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.taskmaster.R;
 public class AddTaskActivity extends AppCompatActivity {
     @Override
-    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        Button btnSave = findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lógica para guardar tarea
+                finish(); // Vuelve a MainActivity
+            }
+        });
+    }
 }
