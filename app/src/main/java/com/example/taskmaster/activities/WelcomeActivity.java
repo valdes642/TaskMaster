ac5f73ec9627029b3a1106e2a3ec3a62f7c82381
valdes642 <vhvergarav@gmail.com>
2026-09-09 19:08:45 -0300

    xxzzx

--- app/src/main/java/com/example/taskmaster/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/taskmaster/activities/WelcomeActivity.java
@@ -18,12 +18,16 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnEnter = findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Para que no pueda volver atrás a esta pantalla
+            }
         });
     }
 }
