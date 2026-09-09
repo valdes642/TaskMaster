43c949c84eaeb416772c10df0ddf79d60e889948
valdes642 <vhvergarav@gmail.com>
2026-09-09 19:41:00 -0300

    estoy muerto

--- app/src/main/java/com/example/taskmaster/activities/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmaster/activities/AddTaskActivity.java
@@ -1,4 +1,9 @@
 package com.example.taskmaster.activities;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.taskmaster.R;
 public class AddTaskActivity {
 }
